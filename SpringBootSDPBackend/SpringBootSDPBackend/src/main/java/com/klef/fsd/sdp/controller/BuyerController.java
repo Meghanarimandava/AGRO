a18c4118cacc3f498b70759e361b806e2c2f12f0
klu2300032597 <2300032597@kluniversity.in>
2025-09-23 15:20:35 +0530

    updated code

--- SpringBootSDPBackend/SpringBootSDPBackend/src/main/java/com/klef/fsd/sdp/controller/BuyerController.java
+++ SpringBootSDPBackend/SpringBootSDPBackend/src/main/java/com/klef/fsd/sdp/controller/BuyerController.java
@@ -19,7 +19,7 @@ import com.klef.fsd.sdp.service.BuyerService;
 public class BuyerController 
 {
    @Autowired
-   private BuyerService buyer
+   private BuyerService buyer;
    @GetMapping("/")
    public String home()
    {
